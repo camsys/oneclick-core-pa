5ba8e0956418ba89c01c423c4294b6cab909c4dc
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-11-14 11:24:21 -0600

    WIP

--- app/services/external_api_ambassadors/otp_ambassador.rb
+++ app/services/external_api_ambassadors/otp_ambassador.rb
@@ -167,14 +167,23 @@ class OTPAmbassador
 
   def convert_itinerary(otp_itin, trip_type)
     associate_legs_with_services(otp_itin)
-
-    Rails.logger.info("Converting OTP itinerary: #{otp_itin.inspect}")
-    Rails.logger.info("Assigned trip type for this itinerary: #{trip_type}")
-
   
-    itin_has_invalid_leg = otp_itin["legs"].detect{ |leg| 
+    Rails.logger.info("Converting OTP itinerary: #{otp_itin.inspect}")
+  
+    # Determine the correct trip_type based on the legs' modes
+    if otp_itin["legs"].all? { |leg| leg["mode"] == "WALK" }
+      trip_type = :walk
+    elsif otp_itin["legs"].any? { |leg| ["BUS", "TRANSIT"].include?(leg["mode"]) }
+      trip_type = :transit
+    end
+  
+    Rails.logger.info("Assigned trip type for this itinerary after leg evaluation: #{trip_type}")
+  
+    # Check for invalid legs (legs with service name but missing service ID)
+    itin_has_invalid_leg = otp_itin["legs"].detect do |leg| 
       leg['serviceName'] && leg['serviceId'].nil?
-    }
+    end
+
     return nil if itin_has_invalid_leg
   
     service_id = otp_itin["legs"]
