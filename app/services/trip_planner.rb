d1ad91d03b2f877d55676cb3a65ba6a25adb6332
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-11-17 16:06:28 -0600

    WIP

--- app/services/trip_planner.rb
+++ app/services/trip_planner.rb
@@ -24,8 +24,9 @@ class TripPlanner
     @except_filters = options[:except_filters] || []
     @filters = @only_filters - @except_filters
 
-    Rails.logger.info("Available transit services: #{@available_services[:transit]}")
-    Rails.logger.info("Available paratransit services: #{@available_services[:paratransit]}")
+    Rails.logger.info("Available services class: #{@available_services.class}")
+    Rails.logger.info("Available services content: #{@available_services.inspect}")
+    
 
 
     @router = OTPAmbassador.new(@trip, @trip_types, @http_request_bundler, @available_services[:transit].or(@available_services[:paratransit]))
