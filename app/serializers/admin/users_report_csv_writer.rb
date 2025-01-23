892d0c8e9df65d0a5e2c50922ad6eeaf664d48c1
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2025-01-23 08:47:21 -0600

    FMRPA-301

--- app/serializers/admin/users_report_csv_writer.rb
+++ app/serializers/admin/users_report_csv_writer.rb
@@ -1,15 +1,12 @@
 module Admin
   class UsersReportCSVWriter < CSVWriter
     columns :email, :first_name, :last_name, 
-            :accommodations, :trips_planned, :paratransit_id, :created_at
-    associations :accommodations, :confirmed_eligibilities, :trips, :preferred_locale   
-
-    def accommodations
-      @record.accommodations.pluck(:code).join(', ')
-    end
+            :trips_planned, :created_at
+    associations :trips, :preferred_locale
     
     def trips_planned
       @record.trips.joins(:ecolane_booking_snapshot).distinct.count
     end    
+
   end
 end
