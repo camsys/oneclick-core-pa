d72efb6894fc0fc367fefd4930567ffc30286c9f
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2025-02-28 13:43:19 -0600

    Fixing bugs

--- app/serializers/admin/booking_snapshots_report_csv_writer.rb
+++ app/serializers/admin/booking_snapshots_report_csv_writer.rb
@@ -15,7 +15,15 @@ module Admin
     end
 
     def disposition_status
-      @record.disposition_status || 'Unknown Disposition'
+      snapshot_status = @record.disposition_status
+      associated_trip = @record.trip
+
+      # If the snapshot status is "Ecolane booking denial" and the associated trip's disposition is also "Ecolane booking denial", return that
+      if snapshot_status == Trip::DISPOSITION_STATUSES[:ecolane_denied] && associated_trip && associated_trip.disposition_status == Trip::DISPOSITION_STATUSES[:ecolane_denied]
+        return Trip::DISPOSITION_STATUSES[:ecolane_denied]
+      else
+        return snapshot_status || 'Unknown Disposition'
+      end
     end
 
     def purpose
