f45a453272604c42ed3dd6bf2f7351ce25e372bf
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2025-03-02 09:57:52 -0600

    WIP

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -110,7 +110,34 @@ class Admin::ReportsController < Admin::AdminController
   end
   
   def trips_table
-    snapshots = EcolaneBookingSnapshot.all
+    @trips = current_user.get_trips_for_staff_user.limit(CSVWriter::DEFAULT_RECORD_LIMIT)
+  
+    @trips = @trips.from_date(@trip_time_from_date).to_date(@trip_time_to_date)
+    @trips = @trips.with_purpose(Purpose.where(id: @purposes).pluck(:name)) unless @purposes.empty?
+    @trips = @trips.origin_in(@trip_origin_region.geom) unless @trip_origin_region.empty?
+    @trips = @trips.destination_in(@trip_destination_region.geom) unless @trip_destination_region.empty?
+    @trips = @trips.oversight_agency_in(@oversight_agency) unless @oversight_agency.blank?
+  
+    if Config.dashboard_mode.to_sym == :travel_patterns && params[:ecolane_denied_trips_only].to_bool
+      @trips = @trips.order(:trip_time).select do |trip|
+        snapshot_status = trip.ecolane_booking_snapshot&.disposition_status
+        error_message = trip.ecolane_booking_snapshot&.ecolane_error_message
+  
+        Rails.logger.info "Checking Trip ID: #{trip.id} (Snapshot Status: #{snapshot_status}, Error Message: #{error_message})"
+  
+        # Only check the snapshot
+        (snapshot_status == "Ecolane booking denial" || !error_message.nil?)
+      end
+  
+      @trips = @trips.sort_by { |trip| trip.trip_time }
+      Rails.logger.info "Total matching trips: #{@trips.count}"
+    else
+      @trips = @trips.order(:trip_time)
+    end
+  
+    trip_ids = @trips.map(&:id)
+  
+    snapshots = EcolaneBookingSnapshot.where(trip_id: trip_ids)
     snapshots = snapshots.where("negotiated_pu >= ?", @trip_time_from_date) if @trip_time_from_date.present?
     snapshots = snapshots.where("negotiated_pu <= ?", @trip_time_to_date) if @trip_time_to_date.present?
     unless @purposes.empty?
@@ -118,11 +145,7 @@ class Admin::ReportsController < Admin::AdminController
       snapshots = snapshots.where(purpose: purpose_names)
     end
   
-    if Config.dashboard_mode.to_sym == :travel_patterns && params[:ecolane_denied_trips_only].to_bool
-      snapshots = snapshots.where("disposition_status = ? OR ecolane_error_message IS NOT NULL", "Ecolane booking denial")
-    else
-      snapshots = snapshots.order("negotiated_pu")
-    end
+    snapshots = snapshots.order("negotiated_pu")
   
     respond_to do |format|
       format.csv { send_data snapshots.to_csv(with: Admin::BookingSnapshotsReportCSVWriter) }
