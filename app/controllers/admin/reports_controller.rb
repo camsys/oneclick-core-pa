afbadeabbe94f7328bd2ef686ea76e9c1eaa2193
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2025-03-02 10:02:56 -0600

    WIP

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -119,9 +119,8 @@ class Admin::ReportsController < Admin::AdminController
     @trips = @trips.oversight_agency_in(@oversight_agency) unless @oversight_agency.blank?
   
     if Config.dashboard_mode.to_sym == :travel_patterns && params[:ecolane_denied_trips_only].to_bool
-      @trips = @trips.joins(:ecolane_booking_snapshot)
-      .where(ecolane_booking_snapshots: { disposition_status: "Ecolane booking denial" })
-      .order(:trip_time)
+      @trips = @trips.order(:trip_time).select do |trip|
+        snapshot_status = trip.ecolane_booking_snapshot&.disposition_status
       end
   
       @trips = @trips.sort_by { |trip| trip.trip_time }
