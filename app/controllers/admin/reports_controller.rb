109d31f7cbae611e4a6d09236e6e3c8a329bebc9
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2025-03-21 09:46:00 -0500

    FMRPA-307, 306

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -124,22 +124,20 @@ class Admin::ReportsController < Admin::AdminController
   
       # Build snapshots
       if Config.dashboard_mode.to_sym == :travel_patterns && params[:ecolane_denied_trips_only].to_bool
-      @trips = @trips.joins(:ecolane_booking_snapshot)
-                      .where(ecolane_booking_snapshots: { disposition_status: "Ecolane booking denial" })
-                      .order(:trip_time)
-      trip_ids = @trips.pluck(:id)
-      snapshots = EcolaneBookingSnapshot.where(trip_id: trip_ids, disposition_status: "Ecolane booking denial")
-    else
-      @trips = @trips.order(:trip_time)
-      trip_ids = @trips.pluck(:id)
-      snapshots = EcolaneBookingSnapshot.where(trip_id: trip_ids)
-      snapshots = snapshots.where("negotiated_pu >= ?", @trip_time_from_date) if @trip_time_from_date.present?
-      snapshots = snapshots.where("negotiated_pu <= ?", @trip_time_to_date) if @trip_time_to_date.present?
-      unless @purposes.empty?
-        purpose_names = Purpose.where(id: @purposes).pluck(:name)
-        snapshots = snapshots.where(purpose: purpose_names)
+        @trips = @trips.joins(:ecolane_booking_snapshot)
+                        .where(ecolane_booking_snapshots: { disposition_status: "Ecolane booking denial" })
+                        .order(:trip_time)
+        trip_ids = @trips.pluck(:id)
+        snapshots = EcolaneBookingSnapshot.where(trip_id: trip_ids, disposition_status: "Ecolane booking denial")
+      else
+        @trips = @trips.order(:trip_time)
+        trip_ids = @trips.pluck(:id)
+        snapshots = EcolaneBookingSnapshot.where(trip_id: trip_ids)
+        unless @purposes.empty?
+          purpose_names = Purpose.where(id: @purposes).pluck(:name)
+          snapshots = snapshots.where(purpose: purpose_names)
+        end
       end
-    end
   
     respond_to do |format|
       format.csv { send_data snapshots.to_csv(with: Admin::BookingSnapshotsReportCSVWriter) }
